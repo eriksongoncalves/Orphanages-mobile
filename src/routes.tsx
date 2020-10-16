afb64f421adee2e66f6f88c2794bf10a316c6625
Erikson Gonçalves <eriksongoncalves@yahoo.com.br>
2020-10-16 14:10:39 -0300

    feat: add other pages and header component

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/display-name */
 import React from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -5,15 +6,49 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import OrphanagesMap from './pages/OrphanagesMap';
 import OrphanageDetails from './pages/OrphanageDetails';
+import SelectMapPosition from './pages/CreateOrphanage/SelectMapPosition';
+import OrphanageData from './pages/CreateOrphanage/OrphanageData';
+
+import Header from './components/Header';
 
 const Stack = createStackNavigator();
 
 function Routes() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+          cardStyle: {
+            backgroundColor: '#f2f3f5'
+          }
+        }}
+      >
         <Stack.Screen name="OrphanagesMap" component={OrphanagesMap} />
-        <Stack.Screen name="OrphanageDetails" component={OrphanageDetails} />
+        <Stack.Screen
+          name="OrphanageDetails"
+          component={OrphanageDetails}
+          options={{
+            headerShown: true,
+            header: () => <Header showCancel={false} title="Orfanato" />
+          }}
+        />
+        <Stack.Screen
+          name="SelectMapPosition"
+          component={SelectMapPosition}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Selecione no mapa" />
+          }}
+        />
+        <Stack.Screen
+          name="OrphanageData"
+          component={OrphanageData}
+          options={{
+            headerShown: true,
+            header: () => <Header title="Informe os dados" />
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
